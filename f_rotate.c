fca045cfd0023c456dc88c4ad74ddf2ad851eb55
leonemunyao <leonemunyao@gmail.com>
2023-12-18 14:57:10 +0300

    f_rotate.c: Rotates the stack from bottom to top

---
+++ f_rotate.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * f_rotate - Rotates the stack
+ * @head: head of the stack
+ * @counter: line number of script being executed
+*/
+void f_rotate(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+stack_t *copy_str;
+copy_str = *head;
+
+if (*head == NULL || (*head)->next == NULL)
+{
+return (0);
+}
+while (copy_str->next)
+{
+copy_str = copy_str->next;
+}
+copy_str->next = *head;
+copy_str->prev->next = NULL;
+copy_str->next = NULL;
+(*head)->prev = copy_str;
+(*head) = copy_str;
+
+}
